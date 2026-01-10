7b13e1174cb5ee0e1d35b7df5713bfcf5819b376
erkang zhou <3226778019@qq.com>
2026-01-10 14:04:16 +0800

    02_number input

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 ## Mastering RShiny Learning Record
 
 ### 01_introduction
-
+#### 01.01 第一个RShiny APP
 学习了基本元素：selectInput、verbatimTextOutput、tableOutput
 学习了数据互联：reactive
 并且通过将 dataset 提取成公共变量，优化了代码
+
+#### 01.02 基本控件元素
+文本控件：textInput、passwordInput、textAreaInput
--- app.R
+++ app.R
@@ -3,7 +3,11 @@ library(shiny)
 ui <- fluidPage(
     textInput(inputId = "username", label = "What's your name?"),
     passwordInput(inputId = "password", label = "Please enter your password"),
-    textAreaInput(inputId = "desc", label = "Self Description")
+    textAreaInput(inputId = "desc", label = "Self Description"),
+    
+    numericInput(inputId = "age", label = "Age", value = 18, min = 16, max = 100),
+    sliderInput(inputId = "height", label = "Height", value = 165, min = 150, max = 200),
+    sliderInput(inputId = "weight", label = "Weight", value = c(100, 120), min = 90, max = 200)
 ) 
     
 server <- function(input, output, session) {
